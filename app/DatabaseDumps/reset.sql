458acfb46276de286aa11251b635a7a995c31fc0
Armin Pfurtscheller <armin.pfurtscheller@googlemail.com>
2017-10-18 12:00:44 +0200

    Added command for db resetting

--- app/DatabaseDumps/reset.sql
+++ app/DatabaseDumps/reset.sql
@@ -1,29 +1,26 @@
-DELETE FROM `efit_admin`;
-ALTER TABLE `efit_admin` AUTO_INCREMENT = 1;
-
 DELETE FROM `efit_migrations`;
 ALTER TABLE `efit_migrations` AUTO_INCREMENT = 1;
 
-DELETE FROM `efit_result`;
-ALTER TABLE `efit_result` AUTO_INCREMENT = 1;
-
-DELETE FROM `efit_stats`;
-ALTER TABLE `efit_stats` AUTO_INCREMENT = 1;
-
-DELETE FROM `efit_task`;
-ALTER TABLE `efit_task` AUTO_INCREMENT = 1;
+DELETE FROM `refresh_tokens`;
+ALTER TABLE `refresh_tokens` AUTO_INCREMENT = 1;
 
 DELETE FROM `efit_ticket`;
 ALTER TABLE `efit_ticket` AUTO_INCREMENT = 1;
 
+DELETE FROM `efit_stats`;
+ALTER TABLE `efit_stats` AUTO_INCREMENT = 1;
+
+DELETE FROM `efit_result`;
+ALTER TABLE `efit_result` AUTO_INCREMENT = 1;
+
 DELETE FROM `efit_user`;
 ALTER TABLE `efit_user` AUTO_INCREMENT = 1;
 
-DELETE FROM `efit_workshop`;
-ALTER TABLE `efit_workshop` AUTO_INCREMENT = 1;
+DELETE FROM `efit_task`;
+ALTER TABLE `efit_task` AUTO_INCREMENT = 1;
 
 DELETE FROM `efit_workshop`;
 ALTER TABLE `efit_workshop` AUTO_INCREMENT = 1;
 
-DELETE FROM `refresh_tokens`;
-ALTER TABLE `refresh_tokens` AUTO_INCREMENT = 1;
+DELETE FROM `efit_admin`;
+ALTER TABLE `efit_admin` AUTO_INCREMENT = 1;
